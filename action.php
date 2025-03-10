2ab27a0ce8b9151c6b05888e7ce437489fe33914
Benks <github@postelio.de>
2025-03-10 23:06:59 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,7 +1,7 @@
 <?php
 if (!defined('DOKU_INC')) die();
 
-class action_plugin_toc_shortcode extends DokuWiki_Action_Plugin {
+class action_plugin_tocshortcode extends DokuWiki_Action_Plugin {
 
     public function register(Doku_Event_Handler $controller) {
         $controller->register_hook('PARSER_WIKITEXT_PREPROCESS', 'BEFORE', $this, 'replace_inhalt');
@@ -9,7 +9,7 @@ class action_plugin_toc_shortcode extends DokuWiki_Action_Plugin {
 
     public function replace_inhalt(Doku_Event $event) {
         // Dein gewünschter Ersatztext
-        $replacement = '<nspages -sidebar -hideNoPages -hideNoSubns -simpleList -h1 -subns -exclude:[start impressum: datenschutzerklaerung:] -textNS="" -textPages="">';
+        $replacement = '<nspages -sidebar -hideNoPages -hideNoSubns -simpleList -h1 -subns -exclude:[start sidebar] -textNS="" -textPages="">';
 
         // Ersetze <inhalt> durch den gewünschten Text
         $event->data = str_replace('{inhaltsverzeichnis}', $replacement, $event->data);
